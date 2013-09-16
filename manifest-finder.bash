6c631c0957920602555228dde334f2166cc58901
Rovanion Luckey <rovanion.luckey@gmail.com>
2013-09-16 15:27:35 +0200

    Added the manifest finder script.

---
+++ manifest-finder.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This script will test wether or not a site declares a cache manifest."
+for address in $(cat alexa.sites); do
+    echo "Testing $address"
+  if wget -q -O- "$address" | grep "manifest" > /dev/null 2>&1; then
+    echo "$address har ett cache manifest!"
+  fi
+done
