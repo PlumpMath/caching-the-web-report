f78f8eccf69f2d1f1a4edec64aa9ee179d62e8b0
Rovanion Luckey <rovanion.luckey@gmail.com>
2013-09-25 21:11:42 +0200

    Added fetching of revisit details.

--- fetch-more-data.bash
+++ fetch-more-data.bash
@@ -4,4 +4,5 @@ ls ./Testsidor/ | grep html |  while read filename
 do
   newName="${filename%.*}"
   wget -O ./Testsidor/$newName.details $(grep ">Details</a>" Testsidor/$filename | perl -ne 'print "$1" if m/(http:\/\/www.webpagetest.org\/result\/\w+\/1\/details)\//')
+  wget -O ./Testsidor/$newName.revisit-details $(grep "/1/details/cached" Testsidor/$filename | perl -ne 'print "$1" if m/(http:\/\/www.webpagetest.org\/result\/\w+\/1\/details\/cached)\//')
 done
