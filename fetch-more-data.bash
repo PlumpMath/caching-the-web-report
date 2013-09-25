7a0f90a2273bc91d0187e0142e652c21dbec51f3
Rovanion Luckey <rovanion.luckey@gmail.com>
2013-09-25 20:23:16 +0200

    Fixed the fetch scirpt so that it makes unique names

--- fetch-more-data.bash
+++ fetch-more-data.bash
@@ -2,5 +2,6 @@
 
 ls ./Testsidor/ | grep html |  while read filename
 do
-  wget -O ./Testsidor/$filename.details $(grep ">Details</a>" Testsidor/$filename | perl -ne 'print "$1" if m/(http:\/\/www.webpagetest.org\/result\/\w+\/1\/details)\//')
+  newName="${filename%.*}"
+  wget -O ./Testsidor/$newName.details $(grep ">Details</a>" Testsidor/$filename | perl -ne 'print "$1" if m/(http:\/\/www.webpagetest.org\/result\/\w+\/1\/details)\//')
 done
